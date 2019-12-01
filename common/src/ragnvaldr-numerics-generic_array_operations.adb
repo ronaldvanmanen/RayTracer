cefe6fb79fb6e66281f966bc3a15d621711f1d3d
Ronald van Manen <rvanmanen@gmail.com>
2019-12-01 16:31:57 +0100

    Added package Ragnvaldr.Numerics.Generic_Array_Operations which will contain generic functions for all combinations of scalar, vector, and matrix operations.

---
+++ common/src/ragnvaldr-numerics-generic_array_operations.adb
@@ -0,0 +1,32 @@
+--  Ragnvaldr Systems -- Raytracer
+--
+--  Copyright (C) 2019  Ronald van Manen
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package body Ragnvaldr.Numerics.Generic_Array_Operations is
+
+    function Vector_Vector_Elementwise_Operation
+      (Left  : Left_Vector;
+       Right : Right_Vector) return Result_Vector
+    is
+    begin
+        return R : Result_Vector do
+            for J in R'Range loop
+                R (J) := Operation (Left (J), Right (J - R'First + Right'First));
+            end loop;
+        end return;
+    end Vector_Vector_Elementwise_Operation;   
+
+end Ragnvaldr.Numerics.Generic_Array_Operations;
---
+++ common/src/ragnvaldr-numerics-generic_array_operations.ads
@@ -0,0 +1,37 @@
+--  Ragnvaldr Systems -- Raytracer
+--
+--  Copyright (C) 2019  Ronald van Manen
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package Ragnvaldr.Numerics.Generic_Array_Operations is
+
+    pragma Pure (Generic_Array_Operations);
+        
+    generic
+        type Left_Scalar is private;
+        type Right_Scalar is private;
+        type Result_Scalar is private;
+        type Index_Type is range<>;
+        type Left_Vector is array (Index_Type) of Left_Scalar;
+        type Right_Vector is array (Index_Type) of Right_Scalar;
+        type Result_Vector is array (Index_Type) of Result_Scalar;
+        with function Operation
+          (Left  : Left_Scalar;
+           Right : Right_Scalar) return Result_Scalar;
+    function Vector_Vector_Elementwise_Operation
+      (Left  : Left_Vector;
+       Right : Right_Vector) return Result_Vector;
+
+end Ragnvaldr.Numerics.Generic_Array_Operations;
